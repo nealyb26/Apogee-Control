9e580ffbc1a7882f2e507afb387f60593147f3a9
stroutcg <caleb.g.strout@vanderbilt.edu>
2025-07-08 15:22:19 -0500

    Used for main_rk4.py

--- Rk4_v1.py
+++ Rk4_v1.py
@@ -3,38 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+FREQUENCY = 10
+MAX_TIME = 16
+GRAV_CONST = 9.80665 # gravity (m/s^2)
+COEFF_DRAG = 4 # drag coefficient **can be changed**
+AIR_DENSITY = 1.225 # air density (kg/m^3)
+MASS = 5.53383 - (247.2/1000) # mass (kg) **can be changed**
+
+# Rocket Dimensions
+DIAMETER = 0.1524 # diameter in meters (6 inches) **can be changed**
+AREA = np.pi * (DIAMETER / 2)**2 # Cross-sectional Area (m^2)
+
 def rk4_apogee_predictor(h0, v0):
-    g = 9.80665 # gravity (m/s^2)
     Cd = 4 # drag coefficient **can be changed**
     rho = 1.225 # air density (kg/m^3)
     m = 18.6 # mass (kg) **can be changed**
     
-    # Rocket Dimensions
-    diameter = 0.1524 # diameter in meters (6 inches) **can be changed**
-    A = np.pi * (diameter / 2)**2 # Cross-sectional Area (m^2)
+    dt = 1 / FREQUENCY # time steps for physics
+    max_steps = int(MAX_TIME / dt) # seconds / dt
 
-
-    dt = 0.05 # time steps for physics
-    max_steps = 10000
-
-    h = h0
-    v = v0
+    h = h0 # height in meters
+    v = v0 # velocity in meters per second
     for _ in range(max_steps):
         if v <= 0:
             break
+
         # Fourth order equations
-        k1_v, k1_h = derivatives(v, m, g, Cd, A, rho)
-        k2_v, k2_h = derivatives(v + 0.5*dt*k1_v, m, g, Cd, A, rho)
-        k3_v, k3_h = derivatives(v + 0.5*dt*k2_v, m, g, Cd, A, rho)
-        k4_v, k4_h = derivatives(v + dt*k3_v, m, g, Cd, A, rho)
+        k1_v, k1_h = find_derivatives(v)
+        k2_v, k2_h = find_derivatives(v + 0.5*dt*k1_v)
+        k3_v, k3_h = find_derivatives(v + 0.5*dt*k2_v)
+        k4_v, k4_h = find_derivatives(v + dt*k3_v)
+        
         v += (dt/6)*(k1_v + 2*k2_v + 2*k3_v + k4_v)
         h += (dt/6)*(k1_h + 2*k2_h + 2*k3_h + k4_h)
     return h
 
-def derivatives(v, m, g, Cd, A, rho):
-    drag = 0.5 * Cd * A * rho * v**2 * np.sign(v)
-    dv = -g - drag / m
-    dh = v
+def find_derivatives(velocity):
+    drag = .5 * COEFF_DRAG * AREA * COEFF_DRAG * velocity**2 * np.sign(velocity)
+    dv = -GRAV_CONST - drag / MASS
+    dh = velocity
     return dv, dh
 
 def plot_apogee_prediction(csv_filename):
